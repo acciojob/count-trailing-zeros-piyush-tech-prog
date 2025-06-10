aa52a3ac8c41224f6f831e3c795883b13ec7f401
Piyush Sharma <piyushopsaws@gmail.com>
2025-06-10 21:08:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function trailingZeros(n) {
-  //your JS code here. If required.
+	if(isNan(n) || !n){
+		alert("Enter A valid Number")
+		return
+	}
+  let zeros=0
+	for (let index = 5; index <=n; index*=5) {
+		zeros+=Math.floor(n/i)
+	}
+	return zeros
 }
 
 const input = prompt("Enter a number");
